a0e1efb42b18b6a7da7e8348853e0ece4c1bac79
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-22 14:10:37 +0200

    Create components

---
+++ src/components/FAB.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FAB = () => {
+  return <div>FAB</div>;
+};
+
+export default FAB;
---
+++ src/views/AllContacts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllContacts = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default AllContacts
