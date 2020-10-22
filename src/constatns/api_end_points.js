83c92ac6b49f6d7a6cade43c8a1d499d4cfed260
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-22 14:02:43 +0200

    Add API ENDPOINTS to use 'jsonplaceholder'

---
+++ src/constatns/api_end_points.js
@@ -0,0 +1,26 @@
+const hostname = window && window.location && window.location.hostname;
+const prod = "https://jsonplaceholder.typicode.com/";
+const staging = "https://jsonplaceholder.typicode.com/";
+const dev = "https://jsonplaceholder.typicode.com/";
+let base = prod;
+
+switch (hostname) {
+  case "localhost":
+    /*
+      run any logic you need link setting Env Checkers
+      */
+    base = dev;
+    break;
+  case "vois-contacts-app":
+    base = staging;
+    break;
+  default:
+    base = prod;
+}
+
+const API_ENDPOINTS = {
+  BASE: base,
+  USERS: `${base}/users`,
+};
+
+export default API_ENDPOINTS;
