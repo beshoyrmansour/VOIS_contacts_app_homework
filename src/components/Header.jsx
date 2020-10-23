3e2a531d123c85f91729eff2c78307b9a390939c
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-23 13:06:22 +0200

    Header UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import logo from "../assets/vodafone_logo.svg";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light container">
+      <div className="d-flex justify-content-between align-items-center w-100">
+        <a className="navbar-brand" href="#">
+          <img src={logo} height="50" alt="" />
+        </a>
+      </div>
+
+      <div className="collapse navbar-collapse">
+        <form className="form-inline d-flex flex-row">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
