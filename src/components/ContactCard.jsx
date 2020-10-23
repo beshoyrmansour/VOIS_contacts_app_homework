5c542c238d82884e5c9dba80c1bf098490e2f53c
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-23 14:04:01 +0200

    Contact Card UI

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const ContactCard = () => {
-  return <div>ContactCard</div>;
+const ContactCard = ({ contact, ...props }) => {
+  return contact ? (
+    <div className="card shadow border-0 mb-4">
+      {"name" in contact && (
+        <h5 className="card-title bg-dark text-white text-center p-3">
+          {contact.name}
+        </h5>
+      )}
+      <div className="card-body text-center">
+        {"email" in contact && <p className="card-text">{contact.email}</p>}
+        {"phone" in contact && <p className="card-text">{contact.phone}</p>}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 };
 
 export default ContactCard;
