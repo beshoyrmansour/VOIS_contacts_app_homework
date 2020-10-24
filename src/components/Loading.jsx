d71ef98252a397f41f747c4e079f0b9a6ad3c50b
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-24 06:18:26 +0200

    fix className

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import "./loading.css";
 const Loading = () => {
   return (
     <div className="loader-wrapper">
-      <div class="loader"></div>
+      <div className="loader"></div>
     </div>
   );
 };
