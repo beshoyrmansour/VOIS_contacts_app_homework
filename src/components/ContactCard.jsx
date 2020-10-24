607ed788de00856c41e2cd3c2a9a892a0f89bf43
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-24 08:15:05 +0200

    Go to Details Page

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ContactCard = ({ contact, ...props }) => {
   return contact ? (
     <div className="col-12 col-md-6 col-lg-4 ">
-      <div className="card shadow border-0 mb-4">
+      <Link
+        className="card shadow border-0 mb-4 text-dark text-decoration-none"
+        to={`/${contact.username}`}
+      >
         {"name" in contact && (
           <h5 className="card-title bg-dark text-white text-center p-3">
             {contact.name}
@@ -13,7 +17,7 @@ const ContactCard = ({ contact, ...props }) => {
           {"email" in contact && <p className="card-text">{contact.email}</p>}
           {"phone" in contact && <p className="card-text">{contact.phone}</p>}
         </div>
-      </div>
+      </Link>
     </div>
   ) : (
     <></>
