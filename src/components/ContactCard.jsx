80472f1bb0df7000d51832bc48574d98a184e503
Besho R Mansour <beshoyrmansour@gmail.com>
2020-10-24 16:23:10 +0200

    Set selected contact

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { setSelectedContact } from "../contexts/actions";
+import { store } from "../contexts/store";
 
 const ContactCard = ({ contact, ...props }) => {
+  const { dispatch, state } = useContext(store);
+
   return contact ? (
-    <div className="col-12 col-md-6 col-lg-4 ">
+    <div
+      className="col-12 col-md-6 col-lg-4 "
+      onClick={() => setSelectedContact(contact)(dispatch)}
+    >
       <Link
         className="card shadow border-0 mb-4 text-dark text-decoration-none"
-        to={`/${contact.username}`}
+        to={`/${contact.id}`}
       >
         {"name" in contact && (
           <h5 className="card-title bg-dark text-white text-center p-3">
@@ -25,3 +32,5 @@ const ContactCard = ({ contact, ...props }) => {
 };
 
 export default ContactCard;
+
+
